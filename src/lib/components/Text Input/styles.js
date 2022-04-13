86b784688b1795a5b6b44ca78a4a96717274ccd2
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-04-13 19:13:50 +0200

    styles for text input

--- src/lib/components/Text Input/styles.js
+++ src/lib/components/Text Input/styles.js
@@ -1,7 +1,7 @@
 export const textInputWrapper = props => {
   return [
     {
-      classes: '',
+      classes: 'group',
       styles: '',
       on: true, // default
     },
@@ -16,9 +16,21 @@ export const textInputWrapper = props => {
 export const textInput = props => {
   return [
     {
-      classes: '',
+      classes: 'outline-none appearance-none p-4 pb-1 transition duration-300 ease-out border-b-4 bg-secondary bg-opacity-5 focus:bg-opacity-25 text-neutral-dark rounded w-full',
       styles: '',
       on: true, // default
+    },
+    {
+      on: props.clean == false,
+      classes: 'border-red-300 bg-opacity-[15%] animate-wiggle',
+    },
+    {
+      on: props.clean == true,
+      classes: 'border-secondary focus:border-primary',
+    },
+    {
+      on: props.type == 'text area',
+      classes: 'resize-none h-28 md:h-40 w-full min-w-[100%] block',
     },
     { 
       classes: '', 
@@ -32,7 +44,7 @@ export const textInput = props => {
 export const textInputName = props => {
   return [
     {
-      classes: '',
+      classes: 'text-secondary font-bold group-focus-within:text-primary capitalize py-0.5 transition duration-300 ease-out',
       styles: '',
       on: true, // default
     },
@@ -47,7 +59,7 @@ export const textInputName = props => {
 export const textInputError = props => {
   return [
     {
-      classes: '',
+      classes: 'text-red-300 font-bold',
       styles: '',
       on: true, // default
     },
