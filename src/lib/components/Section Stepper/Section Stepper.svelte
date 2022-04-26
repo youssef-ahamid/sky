b044e44975c4df1ff6e0f85928b8f3284f19919c
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-04-26 11:56:40 +0200

    fix syntax error

--- src/lib/components/Section Stepper/Section Stepper.svelte
+++ src/lib/components/Section Stepper/Section Stepper.svelte
@@ -6,7 +6,7 @@
     import Go from '$lib/components/Go/Go.svelte'
     
     $: current === steps.length? current = current - 1: ''
-    $: nextStep = `#${steps[current + 1]`
+    $: nextStep = `#${steps[current + 1]}`
 </script>
 
 <Go bind:to={nextStep}>
