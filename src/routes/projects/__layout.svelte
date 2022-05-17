60f4c1476b5acaa1a0381c070922b3db324bfa7c
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-17 17:23:33 +0200

    fix contact us duplication

--- src/routes/projects/__layout.svelte
+++ src/routes/projects/__layout.svelte
@@ -1,27 +1,7 @@
-<script context="module">
-  export const prerender = true
-  import { getPage } from '$lib/gql'
-
-  export async function load() {
-    let page = await getPage('projects')
-
-    return {
-      props: {
-        page,
-      },
-    }
-  }
-</script>
-
 <script>
-  import Contact from '$lib/sections/Contact.svelte'
-
   import { activePageSlug } from '$lib/stores'
-
-  export let page
 
   $activePageSlug = 'projects'
 </script>
 
 <slot />
-<Contact {...page.sections[3]} />
