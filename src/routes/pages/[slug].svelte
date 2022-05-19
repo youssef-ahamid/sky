23d6fefe55ba357884544b2a740be439511c6a4d
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-19 14:03:31 +0200

    projects page illustrations

---
+++ src/routes/pages/[slug].svelte
@@ -0,0 +1,9 @@
+<script>
+import FileInput from "$lib/components/File Input/File Input.svelte";
+
+
+</script>
+
+<div class="min-h-screen flex items-center justify-center">
+    <FileInput />
+</div>
