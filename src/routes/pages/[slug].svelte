1efa13257ec164eea056ef4f9010595bd0fb1a51
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-19 15:37:01 +0200

    remove file input ref

--- src/routes/pages/[slug].svelte
+++
@@ -1,9 +0,0 @@
-<script>
-import FileInput from "$lib/components/File Input/File Input.svelte";
-
-
-</script>
-
-<div class="min-h-screen flex items-center justify-center">
-    <FileInput />
-</div>
