35f2ffa79685ee95709bb3aaf191fe28641248a2
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-19 05:17:01 +0200

    404 fixes

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -8,16 +8,30 @@
 
 <script>
   export let error, status
+  import Button from '$lib/components/Button/Button.svelte'
+  import Go from '$lib/components/Go/Go.svelte'
 </script>
 
 <svelte:head>
   <title>oops! - Status {status}</title>
 </svelte:head>
 
-<div class="w-screen h-screen flex flex-wrap justify-center items-center p-4">
+<div
+  class="w-screen h-screen flex flex-wrap justify-center items-center p-4"
+>
   <div class="text-center">
-    <h1>page under construction!</h1>
     <h2 class="text-primary pb-12">{status}</h2>
+    <h1>Got lost?</h1>
+    <br />
     <p>{error.message}</p>
+    <br />
+    <Go to="/" className="text-primary m-3 flex justify-center w-full"
+      >
+      <Button
+        shape="ghost"
+        type="primary"
+        label="back to home"
+      />
+    </Go>
   </div>
 </div>
