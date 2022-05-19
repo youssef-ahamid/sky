951c71f5bcebc6de0a74f74be429877808f4b4f8
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-19 02:35:29 +0200

    remove extraneous imports

--- src/lib/sections/Quote.svelte
+++ src/lib/sections/Quote.svelte
@@ -1,16 +1,11 @@
 <script>
   import Animateonenterview from '$lib/components/Animate On Enter View/animate on enter view.svelte'
-  import Button from '$lib/components/Button/Button.svelte'
-  import Go from '$lib/components/Go/Go.svelte'
   import Image from '$lib/components/Image/Image.svelte'
   import Person from '$lib/components/Person/Person.svelte'
-  import { getComponentData } from '$lib/helpers'
 
   import Section from '$lib/sections/Section.svelte'
-  import { onMount } from 'svelte'
 
   export let data = {}
-  console.log(data.content)
   let q = data.content[0]
 </script>
 
