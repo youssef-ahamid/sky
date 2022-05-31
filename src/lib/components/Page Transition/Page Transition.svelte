80577b23af21163e9c770789ace95d118ec59418
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-31 15:58:07 +0200

    invert page transition colors

--- src/lib/components/Page Transition/Page Transition.svelte
+++ src/lib/components/Page Transition/Page Transition.svelte
@@ -18,28 +18,28 @@
 
 {#if !isPageLoaded || start}
   <div
-    class="fixed w-screen h-screen inset-0 z-[9999] bg-primary flex flex-col items-center justify-center"
+    class="fixed w-screen h-screen inset-0 z-[9999] bg-neutral-light flex flex-col items-center justify-center"
     in:slide={{ duration: 300 }}
     out:slide={{ duration: 500, delay: 1400 }}
   >
     <div class="flex justify-center items-center">
       <span
-        class="text-neutral-light py-2 w-24 md:w-80 my-8"
-        in:slide={{ duration: 200, delay: 400 }}
-        out:fade={{ duration: 300, delay: 600 }}
+        class="py-2 w-24 md:w-80 my-8"
+        in:slide={{ duration: 200 }}
+        out:fade={{ duration: 300, delay: 1000 }}
       >
-        <Logo className="animate-pulse" />
+        <Logo color="primary" />
       </span>
     </div>
     <span
-      class="rounded-full border-x-2 border-neutral-light animate-spin w-12 h-12 bg-transparent pb-12"
+      class="rounded-full border-x-2 border-primary animate-pulse animate-spin w-12 h-12 bg-transparent pb-12"
       transition:slide={{ duration: 400, delay: 200 }}
     />
     <div
-      class="h-4 w-full flex justify-center transition duration-300 ease-out"
+      class="h-4 w-full flex justify-center transition duration-300 ease-out mt-6"
     >
       <h2
-        class="text-neutral-light text-center mx-auto max-w-[90%] md:max-w-md"
+        class="animate-pulse text-neutral-dark text-center mx-auto max-w-[90%] md:max-w-md"
       >
         <slot />
       </h2>
