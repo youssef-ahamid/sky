b8d97d96ab12c777f4daf3aae1ba1f964fb4927a
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-06-04 22:27:33 +0200

    decrease spacing

--- src/lib/components/Product/Product.svelte
+++ src/lib/components/Product/Product.svelte
@@ -14,10 +14,10 @@
 
 <div class="w-full my-4 flex flex-col space-y-1 md:space-y-4 text-left">
   {#if !!image}
-    <Image className="w-80 md:w-96 h-80 md:h-96" type="custom" src={image.url} />
+    <Image className="w-80 md:w-96 h-80 md:h-96 p-4" type="custom" src={image.url} />
   {/if}
   <div class="px-1">
-    <h2 class="text-neutral-dark">{title}</h2>
+    <h2 class="text-neutral-dark max-w-xs">{title}</h2>
     <div class="flex py-1 space-x-4 md:space-x-6">
       {#if heating}
         <div class="flex space-x-2 uppercase text-red">
